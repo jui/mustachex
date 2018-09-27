dd5e64f7306c01449702757754c85aedc02aa10f
Jussi Uusi-Illikainen <jussiui@gmail.com>
2018-09-28 00:17:26 +0300

    Added ex-doc dev dependency

--- mix.exs
+++ mix.exs
@@ -27,7 +27,7 @@ defmodule Mustachex.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    []
+    [{:ex_doc, ">= 0.0.0", only: :dev}]
   end
 
   defp description do
